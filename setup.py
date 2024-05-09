e821d11d436ccc94e815a6fbd51c47d36379b91e
drewschaub <andrew.schaub@protonmail.com>
2024-05-09 18:05:42 -0400

    updating

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='protein-design-tools',
-    version='0.1.9',
+    version='0.1.10',
     author='Andrew Schaub',
     author_email='andrew.schaub@protonmail.com',
     description='A library of tools for protein design.',
