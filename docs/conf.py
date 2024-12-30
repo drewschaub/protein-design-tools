e0ff3bbf348f3d9681ce0168c5d0e55db3d05261
Andrew Schaub <andrew.schaub@protonmail.com>
2024-12-30 02:41:48 -0500

    trying to fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,16 +13,18 @@ release = '0.1.28'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../..'))  # Adjust as needed based on directory structure
+sys.path.insert(0, os.path.abspath('../'))  # Adjust as needed based on directory structure
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc',
-              'sphinx.ext.coverage',
-              'sphinx.ext.napoleon',
-              'sphinx.ext.autodoc.typehints'
-              ]
+extensions = [
+    'sphinx.ext.autodoc',        # Handles automatic documentation
+    'sphinx.ext.napoleon',       # Supports Google-style and NumPy-style docstrings
+    'sphinx.ext.viewcode',       # Adds links to source code
+    'sphinx.ext.autosummary',    # Generates summary tables for modules/classes
+    'sphinx.ext.autodoc.typehints'  # Adds support for type hints
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
