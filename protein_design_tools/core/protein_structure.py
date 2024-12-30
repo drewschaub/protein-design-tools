f96fd42554cbca909e74c0052bd7b7c3fd7367b2
Andrew Schaub <andrew.schaub@protonmail.com>
2024-12-30 01:17:12 -0500

    updated get_coordinates to allow passing chain id

--- protein_design_tools/core/protein_structure.py
+++ protein_design_tools/core/protein_structure.py
@@ -30,6 +30,8 @@ class ProteinStructure:
         self,
         atom_type: str = "all",
         selection: Optional[Dict[str, Union[List[int], List[range]]]] = None,
+        chain: Optional[str] = None,
+        chains: Optional[List[str]] = None,
     ) -> np.ndarray:
         """
         Retrieve coordinates based on atom type and residue selection.
@@ -37,11 +39,22 @@ class ProteinStructure:
         Parameters
         ----------
         atom_type : str, optional
-            Type of atoms to retrieve ("all", "backbone", "CA", "non-hydrogen").
-            Defaults to "all".
+            Type of atoms to retrieve. One of:
+            - 'all' : All atoms
+            - 'backbone' : Only backbone atoms (N, CA, C, O)
+            - 'CA' : Only alpha carbons
+            - 'non-hydrogen' : All non-hydrogen atoms
+            Defaults to 'all'.
         selection : dict, optional
-            Dictionary specifying residue selection per chain,
-            e.g., {'A': [1,2,3], 'B': range(10, 20)}.
+            Dictionary specifying residue selection per chain, e.g.
+            { 'A': [1, 2, 3], 'B': range(10, 20) }.
+            If provided, only those residues are considered for each chain.
+        chain : str, optional
+            A single chain ID to retrieve all residues from. If set, `chains`
+            is ignored. For example, `chain='A'`.
+        chains : list of str, optional
+            Multiple chain IDs to retrieve all residues from. e.g. `chains=['A','B']`.
+            If `chain` is also provided, `chain` takes precedence.
 
         Returns
         -------
@@ -49,17 +62,69 @@ class ProteinStructure:
             Array of coordinates.
         """
 
-        coordinates = []
+        # 1) Merge chain(s) info into a selection dictionary if needed
+        #    so that each specified chain -> all residue IDs
+        chain_selection = {}
+        
+        # If user specified chain=..., override chains
+        if chain is not None:
+            # single chain
+            chain_selection = {chain: None}  # None -> all residues in that chain
+        elif chains is not None:
+            for ch_id in chains:
+                chain_selection[ch_id] = None
+        
+        # 2) If user also provided `selection`, unify with chain_selection
+        #    - If chain_selection is empty, we just parse selection as usual
+        #    - If chain_selection is not empty, it overrides or merges logic
         parsed_selection = parse_residue_selection(selection) if selection else {}
+        
+        # If chain_selection is specified, we build a combined dict
+        # The logic here: if chain X is in chain_selection, that means "all" residues
+        # unless user also gave `selection` for that chain -> intersection
+        if chain_selection:
+            combined_selection = {}
+            for c in self.chains:
+                if c.name in chain_selection:
+                    # If chain_selection[c.name] is None => all residues
+                    # if selection also has c.name => intersection
+                    if c.name in parsed_selection:
+                        # intersection
+                        selection_res = parsed_selection[c.name]
+                        chain_selection_res = [r.res_seq for r in c.residues]  # all
+                        # intersection of the two sets
+                        inter = set(selection_res).intersection(chain_selection_res)
+                        combined_selection[c.name] = list(inter)
+                    else:
+                        # chain specified, but not in parsed_selection => take all
+                        all_res = [r.res_seq for r in c.residues]
+                        combined_selection[c.name] = all_res
+                else:
+                    # chain not specified => skip
+                    pass
+            # Now combined_selection is the new parsed_selection
+            parsed_selection = {
+                ch: sorted(reslist) for ch, reslist in combined_selection.items()
+            }
+        else:
+            # no chain(s) param, so rely solely on the parsed_selection
+            # i.e., if user gave selection = {'A': [1,2,3], ...} we use that
+            # if user gave no selection, parsed_selection is empty => no restriction
+            pass
 
-        for chain in self.chains:
-            if selection and chain.name not in parsed_selection:
+        # 3) Gather coordinates
+        coordinates = []
+        for c in self.chains:
+            # If parsed_selection is non-empty and c.name isn't in it => skip chain
+            if parsed_selection and c.name not in parsed_selection:
                 continue
+
+            # If c.name is in parsed_selection => we have a list of residues
             selected_residues = parsed_selection.get(
-                chain.name, [res.res_seq for res in chain.residues]
+                c.name, [r.res_seq for r in c.residues]
             )
 
-            for residue in chain.residues:
+            for residue in c.residues:
                 if residue.res_seq not in selected_residues:
                     continue
                 for atom in residue.atoms:
@@ -71,4 +136,6 @@ class ProteinStructure:
                         coordinates.append([atom.x, atom.y, atom.z])
                     elif atom_type == "non-hydrogen" and atom.element != "H":
                         coordinates.append([atom.x, atom.y, atom.z])
+        
         return np.array(coordinates)
+
