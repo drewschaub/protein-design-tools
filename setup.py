4c5607ca3486cd9dc90b0917bf775c5f3b1fb06e
Andrew Schaub <aschaub930@gmail.com>
2025-07-14 21:22:58 -0400

    "chore(release): bump version to 0.1.30"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="protein-design-tools",
-    version="0.1.28",
+    version="0.1.30",
     description="A toolkit for protein structure analysis and design.",
     author="Andrew Schaub",
     author_email="andrew.schaub@protonmail.com",
@@ -22,14 +22,17 @@ setup(
     package_dir={"protein_design_tools": "protein_design_tools"},
     python_requires=">=3.6",
     install_requires=[
-        "numpy",
+        "freesasa",
         "h5py",
+        "numpy==1.26.4",
+        "requests",
+        "torch"
         "sphinx_rtd_theme",
         # Add other dependencies here
     ],
     extras_require={
         "jax_cpu": [
-            "jax[cpu]",
+            "jax[cpu]==0.4.25",
         ],
         "jax_cuda12": [
             "jax[cuda12]",
